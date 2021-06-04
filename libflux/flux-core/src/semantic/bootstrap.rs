4c799fb176f2ab416c921223050d8756f8b9f62e
Dan Moran <dmoran@influxdata.com>
2021-06-04 15:58:13 -0400

    fix: replace hard-coded '/' with platform-agnostic constant

--- libflux/flux-core/src/semantic/bootstrap.rs
+++ libflux/flux-core/src/semantic/bootstrap.rs
@@ -3,7 +3,7 @@
 use std::collections::HashSet;
 use std::fs;
 use std::io;
-use std::path::Path;
+use std::path::{MAIN_SEPARATOR, Path};
 
 use crate::ast;
 use crate::parser;
@@ -172,7 +172,7 @@ fn parse_flux_files(path: &Path) -> io::Result<Vec<ast::File>> {
 // Associates an import path with each file
 fn file_map(files: Vec<ast::File>) -> AstFileMap {
     files.into_iter().fold(AstFileMap::new(), |mut acc, file| {
-        let name = file.name.rsplitn(2, '/').collect::<Vec<&str>>()[1].to_string();
+        let name = file.name.rsplitn(2, MAIN_SEPARATOR).collect::<Vec<&str>>()[1].to_string();
         acc.insert(name, file);
         acc
     })
